043637f6e40e7607436ac6a425e99868edeb6d22
Cesar <cesarbarbo80@hotmail.com>
2025-05-03 12:38:36 -0300

    correccion de errores

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precio: number){
+    const nuevoArray = products.filter((p) => {return p.price < precio});
+    return nuevoArray;
+  }
+  
 }
 
 class User {
@@ -21,7 +27,8 @@ class User {
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
+    
     // pista: push no suma muchos items (agrega de a uno)
   }
 }
